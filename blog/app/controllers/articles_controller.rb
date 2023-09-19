aa129e0486c991d6b7b96e9f50af3f4e2661a346
Castlebin <Castlebin@gmail.com>
2023-09-19 11:58:31 +0800

    在 ArticlesController 中编写对应的 show 方法

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all   # Article 继承了 ApplicationRecord，所以也有 all 方法
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
