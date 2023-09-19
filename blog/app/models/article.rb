8c6d98223717f4a4b9ab4d0b379f740e9d3c66fa
Castlebin <Castlebin@gmail.com>
2023-09-19 17:08:31 +0800

    dependent: :destroy   关联删除

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   include Visible
 
-  has_many :comments  # 在 Article 侧，增加对 Article 的关联。表示一个 Article 可以有多个 Comment
+  #has_many :comments  # 在 Article 侧，增加对 Article 的关联。表示一个 Article 可以有多个 Comment
+  has_many :comments, dependent: :destroy    # 删除文章时，关联删除关联的评论 
 
   validates :title, presence: true    # title 必须存在
   validates :body, presence: true, length: { minimum: 10 }  # body 必须存在，长度最少要有 10
