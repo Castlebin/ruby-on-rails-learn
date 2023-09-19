651cb5d4cd9375f567be29ad45b0ea51ce33baa7
Castlebin <Castlebin@gmail.com>
2023-09-19 16:41:18 +0800

    提取重复的逻辑到 Concern 中，进行重用

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,15 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments  # 在 Article 侧，增加对 Article 的关联。表示一个 Article 可以有多个 Comment
 
   validates :title, presence: true    # title 必须存在
-  validates :body, presence: true, length: { minimum: 10 }  # body 必须存在，长度最少要有 10 
-
-  # 增加对 status 字段的校验
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }   # status 的值必须是 VALID_STATUSES 中之一
-
-  def archived?
-    status == 'archived'
-  end
+  validates :body, presence: true, length: { minimum: 10 }  # body 必须存在，长度最少要有 10
 end
--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,12 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :article     # 生成的对 Article 的关联，表示一个 Comment 一定属于一个 Article 
+  include Visible
 
-  # 增加对 status 字段的校验
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }  # status 的值必须是 VALID_STATUSES 中之一
-
-  def archived?
-    status == 'archived'
-  end
+  belongs_to :article     # 生成的对 Article 的关联，表示一个 Comment 一定属于一个 Article
 end
