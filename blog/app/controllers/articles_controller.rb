8b386d61b7ea2a6b6750c83c4bfaa4ddbce7d2a5
Castlebin <Castlebin@gmail.com>
2023-09-19 13:33:13 +0800

    使用 article_params 作参数校验

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -16,9 +16,8 @@ class ArticlesController < ApplicationController
   # create 操作使用标题和正文值实例化一篇新文章，并尝试保存它。
   # 如果文章保存成功，该操作会将浏览器重定向到位于 "http://localhost:3000/articles/#{@article.id}" 的文章页面。
   # 否则，该操作将通过呈现 app/views/articles/new.html.erb 来重新显示表单，状态代码为 422 无法处理的实体。
-  # 这里的标题和正文是虚拟值。创建表单后，我们将返回并更改这些。
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)  # 使用 article_params 作参数校验，只允许传递 article_params 定义的参数
 
     if @article.save
       redirect_to @article
@@ -26,4 +25,10 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  # 使用 article_params 作参数校验
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
