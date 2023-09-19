38f3aafbf89ed35f790c754854aaafa568d7644f
Castlebin <Castlebin@gmail.com>
2023-09-19 11:11:14 +0800

    将路由 / 设置为 articles#index。这样访问主页直接会映射到 articles#index

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "articles#index"   # 将路由 / 设置为 articles#index。这样访问主页直接会映射到 articles#index
+
   # 声明 GET /articles 请求映射到 ArticlesController 的 index 操作（方法）。
   get "/articles", to: "articles#index"
 
