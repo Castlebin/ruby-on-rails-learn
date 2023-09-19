2738d82b0af9ad376823de83c1bc4cfe9f09c494
Castlebin <Castlebin@gmail.com>
2023-09-19 10:56:45 +0800

    声明 GET /articles 请求映射到 ArticlesController 的 index 操作（方法）

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # 声明 GET /articles 请求映射到 ArticlesController 的 index 操作（方法）。
+  get "/articles", to: "articles#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
